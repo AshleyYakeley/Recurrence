5b5604ff724a7056df019f0436a538d88065b7fc
Ashley Yakeley <ashley@semantic.org>
2012-01-02 00:24:27 -0800

    beginnings of Phase
    Ignore-this: f1cec6ef1754a03763eabea73d91d5f7
    
    darcs-hash:20120102082427-ac6dd-402f85597b79e619e7e9bd120385bd1b8e0827b8

--- src/PhaseSet.hs
+++ src/PhaseSet.hs
@@ -1,4 +1,18 @@
 module PhaseSet where
 {
-    import StationSet
+    import StationSet;
+    
+    data Phase a = MkPhase (KnownStationSet a) (StationSet a);
+
+    -- S+S+E-E-S+E
+    phaseMember :: (Ord a) => Phase a -> a -> Bool;
+    phaseMember (MkPhase s1 s2) t = case kssLastBefore s1 t of
+    {
+        Nothing -> False; -- never started
+        Just start -> case ssLastBeforeUntil s2 t start of
+        {
+            Just _ -> False; -- ended strictly before t on or after start (but shouldn't be on)
+            Nothing -> True; -- didn't end after start
+        };
+    };
 }
