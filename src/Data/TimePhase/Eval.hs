a718b5c2b8ec4bc09be2b984571f8cca9fb82ead
Ashley Yakeley <ashley@semantic.org>
2012-01-16 00:41:25 -0800

    let
    Ignore-this: 1a5df9ab695ec6f414896ebca87b90c4
    
    darcs-hash:20120116084125-ac6dd-3a4aa3f136c6ff46d004a22c40b1bf734078de74

--- src/Data/TimePhase/Eval.hs
+++ src/Data/TimePhase/Eval.hs
@@ -1,9 +1,10 @@
 module Data.TimePhase.Eval(evalWithDict) where
 {
+    import Data.Traversable;
+    import Control.Monad;
     import Language.SExpression;
     import Data.TimePhase.Value;
     import Data.TimePhase.Dict;
-    
     
     eval :: (?evalatom :: String -> Maybe Value) => SExpression String -> M Value;
     eval (AtomSExpression atom) = case ?evalatom atom of
@@ -12,6 +13,31 @@ module Data.TimePhase.Eval(evalWithDict) where
         _ -> reportError ("unknown: " ++ (show atom));
     };
     eval (ListSExpression []) = reportError "empty list";
+    eval (ListSExpression (AtomSExpression "let":rest)) = case rest of
+    {
+        [ListSExpression bindingPairs,subjectExpr] -> do
+        {
+            bindings <- traverse (\bindingPair -> case bindingPair of
+            {
+                ListSExpression [AtomSExpression name,valueExpr] -> do
+                {
+                    value <- eval valueExpr;
+                    return (name,value);
+                };
+                _ -> reportError "badly-formed bindings in let";
+            }) bindingPairs;
+            let
+            {
+                ?evalatom = \s -> mplus (findInPairs bindings s) (?evalatom s);
+            } in eval subjectExpr;
+        };
+        _ -> reportError "badly-formed let"
+    } where
+    {
+        findInPairs [] s = Nothing;
+        findInPairs ((key,value):_) s | key == s = Just value;
+        findInPairs (_:rest) s = findInPairs rest s;
+    };
     eval (ListSExpression (funexpr:argexprs)) = do
     {
         fun <- eval funexpr;
@@ -19,7 +45,7 @@ module Data.TimePhase.Eval(evalWithDict) where
         {
             FunctionValue f -> do
             {
-                args <- mapM eval argexprs;
+                args <- traverse eval argexprs;
                 f args;
             };
             _ -> reportError "expected function"
