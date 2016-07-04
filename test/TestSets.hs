34e4268b5b5c7d7ded22c620fc4556846b44abb9
Ashley Yakeley <ashley@yakeley.org>
2016-07-03 17:04:40 -0700

    get test building

--- test/TestSets.hs
+++ test/TestSets.hs
@@ -1,15 +1,15 @@
-{-# LANGUAGE FlexibleInstances, ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, ScopedTypeVariables #-}
 module Main where
 {
     import Data.SetSearch;
-     
+
     showBool :: Bool -> Char;
     showBool False = 'F';
     showBool True = 'T';
 
     testOne name expected set = if member set 0 /= expected
      then putStrLn ("FAILED: "++name++"="++[showBool expected]) else return ();
-    
+
     testPS context set1 set2 = let
     {
         v1 = member set1 0;
@@ -20,7 +20,7 @@ module Main where
         testOne (context ++ "intersect") (v1 && v2) (intersect set1 set2);
         testOne (context ++ "diff") (v1 && not v2) (diff set1 set2);
     };
-    
+
     testPoint v1 v2 = let
     {
         set1 :: PointSet Int = if v1 then single 0 else empty;
@@ -39,12 +39,12 @@ module Main where
     {
         doPair :: t -> IO ();
     };
-    
+
     instance DoPair (IO ()) where
     {
         doPair = id;
     };
-    
+
     instance DoPair a => DoPair (Bool -> a) where
     {
         doPair t = do
